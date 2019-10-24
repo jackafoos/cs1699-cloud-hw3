173f9771524ec5e7fb226bc2ec4b060f2bab5cbc
= <jem319@pitt.edu>
2019-10-23 22:38:27 -0400

    Test Files and Hadoop Screenshot
    Signed-off-by: = <jem319@pitt.edu>

---
+++ src/NaiveWordCount.java
@@ -0,0 +1,13 @@
+
+
+public class NaiveWordCount {
+  public static void main(){
+    // TODO: Start Timer
+    // TODO: Get Text Files
+    // TODO: make hashmap
+    // TODO: Read and add to hashmap
+    // TODO: Iterate through hashmap and print word count
+    // TODO: End Timer
+    // TODO: Print Results    
+  }
+}
